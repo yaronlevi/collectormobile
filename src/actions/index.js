df4511451011058d27278dcd0616526f8cc5d0e3
yaronlevi02@gmail.com <yaronlevi02@gmail.com>
2016-06-19 10:36:23 +0300

    adding navigation experimental

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,6 +7,12 @@ export const GET_SALES = 'GET_SALES';
 export const GET_SALES_FINISHED = 'GET_SALES_FINISHED';
 export const SPINNER_SHOW = 'SPINNER_SHOW';
 export const SPINNER_HIDE = 'SPINNER_HIDE';
+export const NAVIGATE = 'NAVIGATE'
+export const NAV_PUSH = 'NAV_PUSH'
+export const NAV_POP = 'NAV_POP'
+export const NAV_JUMP_TO_KEY = 'NAV_JUMP_TO_KEY'
+export const NAV_JUMP_TO_INDEX = 'NAV_JUMP_TO_INDEX'
+export const NAV_RESET = 'NAV_RESET'
 
 const url = "http://collector.co.il/SalesBoard/GetSalesForMobile?page=1";
 
@@ -36,4 +42,42 @@ export function getSales(page) {
     type: GET_SALES,
     payload: page
   }
+}
+
+// *** Action Creators ***
+// The following action creators were derived from NavigationStackReducer
+export function navigatePush(state) {
+	state = typeof state === 'string' ? { key: state, title: state } : state
+	return {
+		type: NAV_PUSH,
+		state
+	}
+}
+
+export function navigatePop() {
+	return {
+		type: NAV_POP
+	}
+}
+
+export function navigateJumpToKey(key) {
+	return {
+		type: NAV_JUMP_TO_KEY,
+		key
+	}
+}
+
+export function navigateJumpToIndex(index) {
+	return {
+		type: NAV_JUMP_TO_INDEX,
+		index
+	}
+}
+
+export function navigateReset(routes, index) {
+	return {
+		type: NAV_RESET,
+		index,
+		routes
+	}
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import AlbumsReducer from './reducer_albums.js';
 import AlbumsListParamsReducer from './reducer_albumsListParamsReducer.js';
 import RoutesReducer from './reducer_routes.js';
 import UIStateReducer from './reducer_uiState.js';
+import NavigationReducer from './reducer_navigation.js';
 
 const rootReducer = combineReducers({
   albums: AlbumsReducer,
   albumsListParams: AlbumsListParamsReducer,
   routes: RoutesReducer,
-  uiState: UIStateReducer
+  uiState: UIStateReducer,
+  navigationState : NavigationReducer
 });
 
 export default rootReducer;
